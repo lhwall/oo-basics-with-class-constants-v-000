4f7107871277a6d9cf32d929be48d8e946b06e97
Lucy Wall <lhwall@gmail.com>
2018-03-28 16:57:37 +0000

    automatically backed up by learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -7,8 +7,9 @@ class Shoe
   def initialize(brand)
     @brand = brand
     
-    if BRANDS.member?(brand) != true
-    BRANDS << brand
+      if BRANDS.member?(brand) != true
+      BRANDS << brand
+    end
   end
 
   def cobble
